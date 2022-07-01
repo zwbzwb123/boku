11ef561bfd4013a6a3e9b4ef60f63ec64c448362
zwbzwb123 <1229379493@qq.com>
2022-07-01 15:14:50 +0800

    项目创建

---
+++ src/main/java/com/zwb/boku/BokuApplication.java
@@ -0,0 +1,14 @@
+package com.zwb.boku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class BokuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BokuApplication.class, args);
+    }
+}
---
+++ src/test/java/com/zwb/boku/BokuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zwb.boku;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BokuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
